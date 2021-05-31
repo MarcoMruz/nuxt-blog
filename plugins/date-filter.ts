7ece322d72f03062748fd52fc025f1325f80286a
Marco Mrúz <marcomruz1@gmail.com>
2021-05-31 16:38:05 +0200

    use Intl api when formatting dates

--- plugins/date-filter.ts
+++ plugins/date-filter.ts
@@ -1,31 +1,11 @@
 import Vue from 'vue'
 
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
-
 const dateFilter = (value: Date) => {
   return formatDate(value)
 }
 
 function formatDate(inputDate: Date) {
-  const date = new Date(inputDate)
-  const year = date.getFullYear()
-  const month = date.getMonth()
-  const day = date.getDate()
-  const formattedDate = day + '. ' + months[month] + ' ' + year
-  return formattedDate
+  return new Intl.DateTimeFormat('sk').format(new Date(inputDate))
 }
 
 Vue.filter('date', dateFilter)
